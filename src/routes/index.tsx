89574d6edac378fb9e42df9e14551eed614b7fbe
Ushira Dineth <ushiradineth@gmail.com>
2023-02-05 20:00:44 +0530

    added basic auth example

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,44 +1,31 @@
-import { getSession } from "@auth/solid-start";
-import { signIn } from "@auth/solid-start/client";
-import { createSignal, onCleanup, Show } from "solid-js";
-import { Navigate, useRouteData } from "solid-start";
-import { createServerData$ } from "solid-start/server";
-import { authOptions } from "./api/auth/[...solidauth]";
+import { refetchRouteData, useRouteData } from "solid-start";
+import { createServerAction$ } from "solid-start/server";
+import { logout } from "~/db/session";
+import { useUser } from "../db/useUser";
 
-export const routeData = () => {
-  return createServerData$(async (_, event) => {
-    const session = await getSession(event.request, authOptions);
+export function routeData() {
+  return useUser();
+}
 
-    return { session: session };
-  });
-};
+export function test() {
+  return useUser();
+}
+
 export default function Home() {
-  const session = useRouteData<typeof routeData>();
-  const [redirectIn, setRedirectIn] = createSignal(3);
-
-  const int = setInterval(() => {
-    setRedirectIn((prev) => prev - 1);
-  }, 1000);
-
-  onCleanup(() => clearInterval(int));
+  const user = useRouteData<typeof routeData>();
+  
+  const [, { Form }] = createServerAction$((f: FormData, { request }) => logout(request));
 
   return (
-    <main>
-      <h1>Home</h1>
-      <Show
-        when={session()?.session}
-        fallback={
-          <>
-            <span>You are not signed in.</span>
-            <button onClick={() => signIn("discord")}>Sign In</button>
-          </>
-        }
-      >
-        <span>Redirecting to protected page in {redirectIn()} seconds...</span>
-        <Show when={redirectIn() <= 0}>
-          <Navigate href="/protected" />
-        </Show>
-      </Show>
+    <main class="w-full p-4 space-y-2">
+      <h1 class="font-bold text-3xl">Hello {user()?.username}</h1>
+      <h3 class="font-bold text-xl">Message board</h3>
+      <button onClick={() => refetchRouteData()}>Refresh</button>
+      <Form>
+        <button name="logout" type="submit">
+          Logout
+        </button>
+      </Form>
     </main>
   );
 }
