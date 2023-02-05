3788c239e8cc4295e1620abf744916c441855c54
Ushira Dineth <ushiradineth@gmail.com>
2023-02-05 20:00:30 +0530

    fixed error page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -6,14 +6,9 @@ export default function NotFound() {
     <main>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <div class="w-full p-4 space-y-2">
+        <h1 class="font-bold text-xl">Page Not Found</h1>
+      </div>
     </main>
   );
 }
