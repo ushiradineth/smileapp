b529cb4cca4f9a64a62ae497a1f7de64f248ce93
Ushira Dineth <ushiradineth@gmail.com>
2023-02-06 11:17:18 +0530

    added screen sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,13 @@ module.exports = {
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-        extend: {},
+        screens: {
+            'mobile': '320px',
+
+            'tab': '768px',
+
+            'web': '1024px',
+        },
     },
     plugins: [],
 }
