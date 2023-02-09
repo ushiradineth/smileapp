4f0a01ac34136a4d624117f7c55ccfbb7df4a94c
Ushira Dineth <ushiradineth@gmail.com>
2023-02-09 14:50:35 +0530

    cleaned up code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,10 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
 import { api } from "../utils/api";
-
 import "../styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
