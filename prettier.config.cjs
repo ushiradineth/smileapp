aa0bb0646d614ecbaf4650e18b1ebf79ba37833c
Ushira Dineth <ushiradineth@gmail.com>
2023-02-11 18:12:50 +0530

    added import sort

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,20 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-    plugins: [require.resolve("prettier-plugin-tailwindcss")],
-    printWidth: 99999
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  printWidth: 99999,
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "^(next-auth/(.*)$)|^(next-auth$)",
+    "^[./components]",
+    "^[./]",
+    "^(react-icons/(.*)$)|^(react-icons$)",
+    "^types$",
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
 };
