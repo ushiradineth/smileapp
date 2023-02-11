1b870481f776256dd86f947d818c87fa031dda29
Ushira Dineth <ushiradineth@gmail.com>
2023-02-11 20:16:01 +0530

    added user routing

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,16 @@
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+import { z } from "zod";
+
+export const userRouter = createTRPCRouter({
+  getUser: protectedProcedure.input(z.object({ id: z.string() })).query(({ input, ctx }) => {
+    return ctx.prisma.user.findUnique({ where: { id: input.id } });
+  }),
+
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.user.findMany();
+  }),
+
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+});
