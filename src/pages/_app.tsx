c76a7108359a2699025ca4d12d11fe123ff77f2f
Ushira Dineth <ushiradineth@gmail.com>
2023-02-11 19:19:48 +0530

    added layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
-import { api } from "../utils/api";
-
+import Layout from "../components/Layout";
 import "../styles/globals.css";
+import { api } from "../utils/api";
 
 const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
   );
 };
