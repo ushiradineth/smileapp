055ad0ba043eca9bd809289e480cfe09a88aff41
Ushira Dineth <ushiradineth@gmail.com>
2023-02-14 21:57:47 +0530

    added basic details

--- src/pages/profile/[profile].tsx
+++ src/pages/profile/[profile].tsx
@@ -1,12 +1,14 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import Error from "../../components/Error";
 import Loader from "../../components/Loader";
 import { api } from "../../utils/api";
-import Image from "next/image";
 import { DefaultUserImage } from "../../utils/default";
+import { Round, User } from "@prisma/client";
 
 const Profile: NextPage = () => {
   const { status } = useSession();
@@ -25,17 +27,65 @@ const Profile: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="min-h-screen w-full bg-red-300">
-          <Image src={profile.data?.image || DefaultUserImage} className={"rounded-full"} height={200} width={200} alt={"User Image"}/>
+        <div className="min-h-screen w-full flex flex-col items-center">
+          <Image src={profile.data?.image || DefaultUserImage} className={"rounded-full"} height={200} width={200} alt={"User Image"} />
           <p>{profile.data?.name}</p>
           <p>{profile.data?.email}</p>
-          <p>{profile.data?.wins.length}</p>
-          <p>{profile.data?.losses.length}</p>
-          <p>{profile.data?.highestStreak}</p>
+          <p>Leaderboard Rank #10 (top 0.01%)</p>
+          <Stats wins={profile.data?.wins || []} losses={profile.data?.losses || []} highestStreak={profile.data?.highestStreak || 0} />
+          <ExtraDetails wins={profile.data?.wins || []} losses={profile.data?.losses || []} />
         </div>
       </main>
     </>
   );
 };
+
+function Stats({ ...props }: { wins: Round[]; losses: Round[]; highestStreak: number }) {
+  return (
+    <>
+      <div className="flex gap-2">
+        <p>{props.wins.length + props.losses.length} Rounds</p>
+        <p>{props.wins.length} Wins</p>
+        <p>{props.losses.length} Losses</p>
+      </div>
+      <p>{Math.round((props.wins.length || 0 / (props.wins.length || 0 + props.losses.length || 0)) * 100)} Win Rate</p>
+      <p>{props.highestStreak}</p>
+    </>
+  );
+}
+
+function ExtraDetails({ ...props }: { wins: Round[]; losses: Round[] }) {
+  let weekGameTime = 0;
+  let allGameTime = 0;
+
+  props.wins.forEach((e) => {
+    if (e.finishedAt.getTime() > Date.now() - 1000 * 60 * 60 * 24 * 7) weekGameTime = weekGameTime + e.finishedAt.getMinutes();
+    allGameTime = allGameTime + e.finishedAt.getMinutes();
+  });
+
+  props.losses.forEach((e) => {
+    if (e.finishedAt.getTime() > Date.now() - 1000 * 60 * 60 * 24 * 7) weekGameTime = weekGameTime + e.finishedAt.getMinutes();
+    allGameTime = allGameTime + e.finishedAt.getMinutes();
+  });
+
+  const processedValues = Object.values(props.wins);
+  const fastestTime = Math.min(...processedValues.map((obj) => obj.timeTaken));
+
+  return (
+    <div className="flex items-center gap-2">
+      <div className="flex flex-col items-center rounded-lg border p-2">
+        <p>Game time</p>
+        <p>Last Week: {weekGameTime}</p>
+        <p>All time: {allGameTime}</p>
+      </div>
+      <div className={`flex flex-col h-fit items-center rounded-lg border p-2 ${fastestTime === Infinity && "hidden"}`}>
+        <p>Fastest round</p>
+        <Link href={props.wins.find((e) => e.timeTaken == fastestTime)?.solutionLink || "/"}>
+          <p>Time: {fastestTime}</p>
+        </Link>
+      </div>
+    </div>
+  );
+}
 
 export default Profile;
