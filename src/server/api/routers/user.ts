401c70549f06d2db7fa4f234d8921ce4ed16f705
Ushira Dineth <ushiradineth@gmail.com>
2023-02-16 00:12:03 +0530

    updated user delete

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,4 +1,6 @@
+import { env } from "../../../env.mjs";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { createClient } from "@supabase/supabase-js";
 import { z } from "zod";
 
 export const userRouter = createTRPCRouter({
@@ -6,14 +8,23 @@ export const userRouter = createTRPCRouter({
     return ctx.prisma.user.findUnique({ where: { id: input.id }, include: { wins: true, losses: true } });
   }),
 
-  deleteUser: protectedProcedure.input(z.object({ id: z.string() })).mutation(({ input, ctx }) => {
-    return ctx.prisma.user.delete({ where: { id: input.id } });
+  deleteUser: protectedProcedure.input(z.object({ id: z.string() })).mutation(async ({ input, ctx }) => {
+    const supabase = createClient(env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_PUBLIC_ANON_KEY);
+    const { error } = await supabase.storage.from(env.NEXT_PUBLIC_SUPABASE_BUCKET).remove([`Users/${input.id}/ProfilePicture`]);
+
+    if (error) return error;
+    else
+      return ctx.prisma.user.delete({
+        where: {
+          id: input.id,
+        },
+      });
   }),
 
   updateName: protectedProcedure.input(z.object({ id: z.string(), name: z.string() })).mutation(({ input, ctx }) => {
     return ctx.prisma.user.update({ where: { id: input.id }, data: { name: input.name } });
   }),
-  
+
   updateImage: protectedProcedure.input(z.object({ id: z.string(), image: z.string().url() })).mutation(({ input, ctx }) => {
     return ctx.prisma.user.update({ where: { id: input.id }, data: { image: input.image } });
   }),
