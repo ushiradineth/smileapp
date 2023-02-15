32e4ab47550e2a9ae63290359285e12ee7188519
Ushira Dineth <ushiradineth@gmail.com>
2023-02-15 22:46:06 +0530

    added name and image update methods

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,31 +1,61 @@
 import { type ChangeEvent, useEffect, useRef, useState } from "react";
 import { type NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import NextImage from "next/image";
 import { useSession } from "next-auth/react";
 import Loader from "../components/Loader";
 import { env } from "../env.mjs";
 import { api } from "../utils/api";
-import { DefaultUserImage } from "../utils/default";
 import { createClient } from "@supabase/supabase-js";
 
 const Settings: NextPage = () => {
   const { data: session } = useSession();
   const [name, setName] = useState(session?.user.name || "");
-  const supabase = createClient(env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_PUBLIC_ANON_KEY);
-  const updateName = api.userRouter.updateName.useMutation();
+  const imageRef = useRef<HTMLInputElement | null>(null);
+  const [image, setImage] = useState<File>();
+  const [imageURL, setImageURL] = useState<File>();
+  const [edited, setEdited] = useState(false);
 
-  const onSubmit = async () => {
-    // let Image = session?.user.image;
-    // if (Name !== session?.user.name || typeof image !== "undefined") {
-    //   if (typeof image !== "undefined" && imageURL) {
-    //     await supabase.storage.from(env.NEXT_PUBLIC_SUPABASE_BUCKET).upload(`/Users/${session?.user.id}/ProfilePicture`, imageURL, {
-    //       cacheControl: "1",
-    //       upsert: true,
-    //     });
-    //     Image = `${env.NEXT_PUBLIC_SUPABASE_IMAGE_URL}Users/${session?.user.id}/ProfilePicture`;
-    //   }
+  const supabase = createClient(env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_PUBLIC_ANON_KEY);
+  const updateName = api.userRouter.updateName.useMutation({onSettled: () => location.reload()});
+  const updateImage = api.userRouter.updateImage.useMutation({onSettled: () => location.reload()});
+  
+  const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+    setEdited(true);
+    setImage(e.target.files[0]);
   };
+
+  const onSave = async () => {
+    if (typeof image !== "undefined" && typeof imageURL !== "undefined") {
+      await supabase.storage.from(env.NEXT_PUBLIC_SUPABASE_BUCKET).upload(`/Users/${session?.user.id}/ProfilePicture`, imageURL, {
+        cacheControl: "1",
+        upsert: true,
+      });
+
+        updateImage.mutate({ id: session?.user?.id || "", image: `${env.NEXT_PUBLIC_SUPABASE_IMAGE_URL}Users/${session?.user.id}/ProfilePicture` });
+    }
+
+    if (name !== session?.user.name) updateName.mutate({ id: session?.user?.id || "", name });
+  };
+
+  useEffect(() => {
+    const img = new (Image as any)();
+    img.src = image ? URL.createObjectURL(image) : session?.user.image;
+    img.onload = async function () {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      canvas.width = 240;
+      canvas.height = 240;
+      ctx?.drawImage(img, 0, 0, 240, 240);
+      const blob = await (await fetch(canvas.toDataURL("image/jpeg"))).blob();
+      const file = new File([blob], "fileName.jpg", { type: "image/jpeg" });
+      setImageURL(file);
+    };
+    img.setAttribute("crossorigin", "anonymous");
+  }, [image]);
 
   if (typeof session === "undefined" || session === null || typeof session.user === "undefined") return <Loader />;
 
@@ -38,16 +68,22 @@ const Settings: NextPage = () => {
       </Head>
       <main>
         <div className="w-full mb-40">
-          <Image src={session.user.image || DefaultUserImage} className={"rounded-full"} height={200} width={200} alt={"User Image"} />
-          <input onChange={(e) => setName(e.target.value)} defaultValue={session.user.name || ""} placeholder="Name" />
-          <button disabled={session?.user.name !== name} onClick={() => updateName.mutate({ id: session?.user.id || "", name })} className="bg-black disabled:bg-blue-300 disabled:cursor-pointer">
+          <NextImage src={URL.createObjectURL(imageURL || new Blob())} className={"rounded-full"} height={200} width={200} alt={"User Image"} />
+          <input type="file" accept=".png, .jpg, .jpeg" ref={imageRef} onChange={handleFileChange} style={{ display: "none" }} />
+          <button className="cursor-pointer text-sm text-blue-400" onClick={() => imageRef.current?.click()}>
+            Change profile picture
+          </button>
+          <input
+            onChange={(e) => {
+              if (Boolean(e.target.value !== session.user.name) !== edited) setEdited(e.target.value !== session.user.name);
+              setName(e.target.value);
+            }}
+            defaultValue={session.user.name || ""}
+            placeholder="Name"
+          />
+          <button disabled={!edited} onClick={() => onSave()} className="disabled:bg-blue-300 disabled:cursor-pointer">
             Save
           </button>
-          {/* <div className="mb-4 h-24 w-full flex items-center justify-center">
-            <button className="cursor-pointer text-sm text-blue-400" onClick={() => imageRef.current?.click()}>
-              Change profile picture
-            </button>
-          </div> */}
         </div>
       </main>
     </>
