458dfeef02a92566d62075151c5e07359f44efd1
Ushira Dineth <ushiradineth@gmail.com>
2023-02-15 12:18:39 +0530

    added font

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -8,7 +8,7 @@ export default function Loader() {
         <meta name="description" content="SmileApp by Ushira Dineth" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-w-screen min-h-screen flex-col items-center justify-center">
+      <main className="flex min-w-screen min-h-screen font-cal flex-col items-center justify-center">
         <p className="h-8 w-16 animate-spin fill-pink-200 text-black dark:text-gray-600 absolute">{"["}</p>
         <p>SMILE</p>
       </main>
