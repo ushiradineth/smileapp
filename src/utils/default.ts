5d782acd5b4ec0e1273d7f8d8bb967bf7182ce07
Ushira Dineth <ushiradineth@gmail.com>
2023-02-16 21:24:56 +0530

    added default background image

--- src/utils/default.ts
+++ src/utils/default.ts
@@ -1,1 +1,2 @@
 export const DefaultUserImage = "https://nockgnasolvxkfacuzmm.supabase.co/storage/v1/object/public/smileapp/Assets/default-user-image.jpg";
+export const DefaultBackgroundImage = "https://nockgnasolvxkfacuzmm.supabase.co/storage/v1/object/public/smileapp/Assets/default-placeholder-image.png";
