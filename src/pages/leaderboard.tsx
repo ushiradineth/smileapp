3a42399c267b410d800e78c7fd0d1596aff20627
Ushira Dineth <ushiradineth@gmail.com>
2023-02-17 19:28:13 +0530

    added padding

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -25,7 +25,7 @@ function Leaderboard() {
         <table className="sm:w-[320px] w-screen md:w-[700px] text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 text-center dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" className="py-3">
+              <th scope="col" className="py-3 px-3">
                 #
               </th>
               <th scope="col" className="py-3">
