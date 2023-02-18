dc1b44ccefef77add76e15d246680e38316d25db
Ushira Dineth <ushiradineth@gmail.com>
2023-02-18 11:18:46 +0530

    added get level methods

--- src/server/api/routers/level.ts
+++ src/server/api/routers/level.ts
@@ -2,7 +2,11 @@ import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { z } from "zod";
 
 export const levelRouter = createTRPCRouter({
-  getLevel: protectedProcedure.input(z.object({ levelid: z.string() })).mutation(({ input, ctx }) => {
-    return ctx.prisma.round.findUnique({ where: { id: input.levelid } });
+  getAllLevels: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.level.findMany({ orderBy: { rounds: { _count: "desc" } } });
+  }),
+
+  getLevel: protectedProcedure.input(z.object({ levelid: z.string() })).query(({ input, ctx }) => {
+    return ctx.prisma.level.findUnique({ where: { id: input.levelid } });
   }),
 });
