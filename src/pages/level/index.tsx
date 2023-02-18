de7c6ef9a3138a3868074b1c4d58ca82e85d353e
Ushira Dineth <ushiradineth@gmail.com>
2023-02-18 11:19:01 +0530

    added basic level index page

--- src/pages/level/index.tsx
+++ src/pages/level/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useSession } from "next-auth/react";
+import { api } from "../../utils/api";
+import Link from "next/link";
 
 function Levels() {
+  const { status } = useSession();
+  const levels = api.levelRouter.getAllLevels.useQuery(undefined, { retry: false, refetchOnWindowFocus: false, enabled: status === "authenticated" });
+
   return (
-    <div>Levels</div>
-  )
+    <>
+      {levels.data?.map((e) => (
+        <div key={e.id}><Link href={"/level/" + e.id}>{e.id}</Link></div>
+      ))}
+    </>
+  );
 }
 
-export default Levels
+export default Levels;
