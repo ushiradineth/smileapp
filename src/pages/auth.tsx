543dc7bc0bd5fa1fc5a85cb2ea744e4ce45fc942
Ushira Dineth <ushiradineth@gmail.com>
2023-03-25 17:31:45 +0530

    moved tabbedmenu

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
-import { TabbedMenu, TabbedMenuBody, TabbedMenuTabLeft, TabbedMenuTabRight, TabbedMenuTabs } from "../components/TabbedMenu";
+import { TabbedMenu, TabbedMenuBody, TabbedMenuTabLeft, TabbedMenuTabRight, TabbedMenuTabs } from "../components/ui/TabbedMenu";
 import { Login } from "../components/auth/Login";
 import { Register } from "../components/auth/Registration";
 import Head from "next/head";
-import { useSession } from "next-auth/react";
 
 const Auth = () => {
   const [loginMenu, setLoginMenu] = useState(true);
-  const { data: session, status } = useSession();
-
-  console.log(session);
 
   return (
     <>
@@ -18,14 +14,15 @@ const Auth = () => {
         <meta name="description" content="SmileApp by Ushira Dineth" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <TabbedMenu>
-        <TabbedMenuTabs>
-          {status}
-          <TabbedMenuTabLeft text={"Login"} state={!loginMenu} onClick={() => setLoginMenu(true)} />
-          <TabbedMenuTabRight text={"Register"} state={loginMenu} onClick={() => setLoginMenu(false)} />
-        </TabbedMenuTabs>
-        <TabbedMenuBody>{loginMenu ? <Login /> : <Register />}</TabbedMenuBody>
-      </TabbedMenu>
+      <div className="mt-24 mb-60">
+        <TabbedMenu>
+          <TabbedMenuTabs>
+            <TabbedMenuTabLeft text={"Login"} state={!loginMenu} onClick={() => setLoginMenu(true)} />
+            <TabbedMenuTabRight text={"Register"} state={loginMenu} onClick={() => setLoginMenu(false)} />
+          </TabbedMenuTabs>
+          <TabbedMenuBody>{loginMenu ? <Login /> : <Register />}</TabbedMenuBody>
+        </TabbedMenu>
+      </div>
     </>
   );
 };
