56fc8762b32f5d98c70031f23f136f5848052d4d
Ushira Dineth <ushiradineth@gmail.com>
2023-03-25 17:33:28 +0530

    fixed auth routing

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import React, { type ReactNode, useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Loader from "./Loader";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/DropdownMenu";
 import { AiFillCaretDown, AiFillGithub, AiFillLinkedin } from "react-icons/ai";
@@ -30,7 +30,8 @@ export default function Layout({ children }: { children: ReactNode }) {
     return () => router.events.off("routeChangeStart", closeMenu);
   }, [router.events]);
 
-  if (status === "unauthenticated" && router.pathname !== "/" && router.pathname !== "/guest") router.push("/");
+  if (status === "unauthenticated" && router.pathname !== "/" && router.pathname !== "/guest" && router.pathname !== "/auth") router.push("/");
+  if (status === "authenticated" && router.pathname !== "/" && router.pathname === "/auth") router.push("/");
   if (status === "loading") return <Loader />;
   if (router.query.initial) {
     location.reload();
@@ -38,18 +39,18 @@ export default function Layout({ children }: { children: ReactNode }) {
   } else if (session?.user.name === "name" && router.pathname !== "/settings") router.push("/settings?setName=true");
 
   return (
-    <main className="flex justify-center h-full relative">
-      <div id="container" className={`font-cal min-h-screen w-full flex flex-col items-center ${CalSans.variable}`}>
-        <div id="navbar" className="border-b bg-white text-black w-full h-fit pt-[25px] pb-[25px] flex items-center px-4 md:px-10 gap-4 md:gap-8 relative max-w-[1024px]">
+    <main className="relative flex h-full justify-center">
+      <div id="container" className={`flex min-h-screen w-full flex-col items-center font-cal ${CalSans.variable}`}>
+        <div id="navbar" className="relative flex h-fit w-full max-w-[1024px] items-center gap-4 border-b bg-white px-4 pt-[25px] pb-[25px] text-black md:gap-8 md:px-10">
           <Link href={"/"} className="z-10">
             <Image src="/logo-black.png" height={70} width={120} className="w-24 md:w-full" alt={"logo"} priority />
           </Link>
-          <div className="flex gap-4 object-right absolute right-4 md:right-10 w-full justify-end">
+          <div className="absolute right-4 flex w-full justify-end gap-4 object-right md:right-10">
             <Link href={"https://github.com/ushiradineth/smileapp"}>
               <Github className="h-6 w-6" />
             </Link>
             <DropdownMenu open={isOpen} onOpenChange={setOpen}>
-              <DropdownMenuTrigger className="select-none outline-none flex items-center">
+              <DropdownMenuTrigger className="flex select-none items-center outline-none">
                 <User className="h-6 w-6" />
                 <AiFillCaretDown className="h-3 w-3" />
               </DropdownMenuTrigger>
@@ -61,18 +62,18 @@ export default function Layout({ children }: { children: ReactNode }) {
                 <DropdownMenuSeparator className={`${status === "unauthenticated" ? "hidden" : ""}`} />
                 <LinkItem link={`/profile/${session?.user.id}`} icon={<User className="h-4 w-4" />} title="Profile" hidden={status === "unauthenticated"} />
                 <LinkItem link="/settings" icon={<Settings className="h-4 w-4" />} title="Settings" hidden={status === "unauthenticated"} />
-                {status === "unauthenticated" ? <ButtonItem onClick={() => signIn()} icon={<LogIn className="h-4 w-4" />} title="Sign In" /> : <ButtonItem onClick={() => signOut()} icon={<LogOut className="h-4 w-4" />} title="Log out" />}
+                {status === "unauthenticated" ? <ButtonItem onClick={() => router.push("/auth")} icon={<LogIn className="h-4 w-4" />} title="Sign In" /> : <ButtonItem onClick={() => signOut()} icon={<LogOut className="h-4 w-4" />} title="Log out" />}
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
         </div>
         <main className="max-w-[1024px]">{children}</main>
-        <div id="footer" className="w-full h-fit bg-black flex justify-center absolute bottom-0">
-          <div className="h-fit max-w-[1024px] w-full text-white flex items-center p-2 text-[10px] md:text-[20px]">
-            <Image src="/logo-white.png" height={70} width={120} className="h-[35px] w-[60px] m-4 md:m-8 md:h-[70px] md:w-[120px]" alt={"logo"} priority />
-            <div className="w-full flex flex-col justify-end m-4 md:m-8">
+        <div id="footer" className="absolute bottom-0 flex h-fit w-full justify-center bg-black">
+          <div className="flex h-fit w-full max-w-[1024px] items-center p-2 text-[10px] text-white md:text-[20px]">
+            <Image src="/logo-white.png" height={70} width={120} className="m-4 h-[35px] w-[60px] md:m-8 md:h-[70px] md:w-[120px]" alt={"logo"} priority />
+            <div className="m-4 flex w-full flex-col justify-end md:m-8">
               <p className="flex justify-end">Ushira Dineth</p>
-              <div className="flex flex-col md:flex-row-reverse md:gap-4 items-end md:items-center">
+              <div className="flex flex-col items-end md:flex-row-reverse md:items-center md:gap-4">
                 <p>2214787</p>
                 <Link href={"mailto:ushiradineth@gmail.com"}>ushiradineth@gmail.com</Link>
                 <div className="flex gap-2">
@@ -95,7 +96,7 @@ export default function Layout({ children }: { children: ReactNode }) {
 function LinkItem({ ...props }: { icon: JSX.Element; title: string; link: string; hidden: boolean }) {
   return (
     <DropdownMenuItem className={`${props.hidden ? "hidden" : ""}`}>
-      <Link href={props.link} className="w-full flex items-center">
+      <Link href={props.link} className="flex w-full items-center">
         <DropdownMenuLabel>{props.icon}</DropdownMenuLabel>
         <p>{props.title}</p>
       </Link>
@@ -106,7 +107,7 @@ function LinkItem({ ...props }: { icon: JSX.Element; title: string; link: string
 function ButtonItem({ ...props }: { icon: JSX.Element; title: string; hidden?: boolean; onClick: (...args: any[]) => unknown }) {
   return (
     <DropdownMenuItem className={`${props.hidden ? "hidden" : ""}`}>
-      <button onClick={props.onClick} className={"flex items-center h-full w-full"}>
+      <button onClick={props.onClick} className={"flex h-full w-full items-center"}>
         <DropdownMenuLabel>{props.icon}</DropdownMenuLabel>
         <p>{props.title}</p>
       </button>
